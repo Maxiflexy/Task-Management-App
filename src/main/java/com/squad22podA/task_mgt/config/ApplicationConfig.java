ef34c332fb58894f03ae02115fb7c45f7afed616
Maxiflexy <onyemax247@gmail.com>
2024-07-13 22:54:51 +0100

    updated AppConfig

--- src/main/java/com/squad22podA/task_mgt/config/ApplicationConfig.java
+++ src/main/java/com/squad22podA/task_mgt/config/ApplicationConfig.java
@@ -45,6 +45,7 @@ public class ApplicationConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
 
+
         return config.getAuthenticationManager();
     }
 }
